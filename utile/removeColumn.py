4a0744ae926e4ac09c2ed8a998d88975335316e6
anas4444 <anas.elabed@etudiant-fst.utm.tn>
2023-09-08 16:17:38 +0100

    changes

--- utile/removeColumn.py
+++ utile/removeColumn.py
@@ -8,26 +8,43 @@ def find_all(input_string, substring):
         index = input_string.find(substring, index + 1)
     return indexes
 
-microservices = ["vehicule", "stocks", "personnel"]
+microservices = ["stocks"]
+restricted = ["Authority", "User", "package-info"]
 paths = ["D:\\Projects\\Xtensus\\"+microservice.capitalize()+"\\src\\main\\java\\com\\xtensus\\"+microservice+"\\domain" for microservice in microservices]
 for path in paths:
     for filename in glob.glob(os.path.join(path, '*.java')):
+        entity = filename.split('\\')[-1]
+        if any(word in entity for word in restricted):
+            continue
         with open(os.path.join(os.getcwd(), filename), 'r') as f:
             data = f.readlines()
             i = -1
+            relationship = False
             for line in data:
                 i += 1
-                if "@Column" not in line:
-                    continue
-                if ("_ar\"" in line):
-                    index2 = line.find("_ar\"")
-                    line = line[:index2+1] + "AR" + line[index2+3:]
-                indexs = find_all(line, "_")
-                for index in indexs:
-                    print(line[index])
-                    line = line[:index] + line[index] + line[index+1].upper() + line[index+2:]
-                line = line.replace('_', '')
-                data[i] = line
+                if "@Column" in line and "_" in line:
+                    if ("_ar\"" in line):
+                        index2 = line.find("_ar\"")
+                        line = line[:index2+1] + "AR" + line[index2+3:]
+                    elif ("_ht\"" in line):
+                        index2 = line.find("_ht\"")
+                        line = line[:index2+1] + "HT" + line[index2+3:]
+                    elif ("_tva\"" in line):
+                        index2 = line.find("_tva\"")
+                        line = line[:index2+1] + "TVA" + line[index2+4:]
+                    elif ("_ttc\"" in line):
+                        index2 = line.find("_ttc\"")
+                        line = line[:index2+1] + "TTC" + line[index2+4:]
+                    indexs = find_all(line, "_")
+                    for index in indexs:
+                        line = line[:index] + line[index] + line[index+1].upper() + line[index+2:]
+                    line = line.replace('_', '')
+                    data[i] = line
+                if "@ManyToOne" in line and "@JoinColumn" not in data[i+1]:
+                    relationship = True
+                if (relationship == True and "private" in line):
+                    relationship = False
+                    data.insert(i-1, f"    @JoinColumn(name=\"{line.split()[-1][:-1]}\", nullable=true)\n")
 
         with open(os.path.join(os.getcwd(), filename), 'w') as f:
             f.writelines(data)
